a44911d69010884a920f5404b2f3880afa1936ef
roxxe34 <hamzafarissi7@gmail.com>
2023-10-16 13:12:54 +0100

    ruby

--- 0x00-challenge/2-sort.rb
+++ 0x00-challenge/2-sort.rb
@@ -11,21 +11,21 @@ ARGV.each do |arg|
 
     # convert to integer
     i_arg = arg.to_i
-    
+
     # insert result at the right position
     is_inserted = false
-    i = 0
-    l = result.size
-    while !is_inserted && i < l do
-        if result[i] < i_arg
-            i += 1
-        else
-            result.insert(i - 1, i_arg)
+
+    # Iterate through the result array to find the correct position
+    result.each_with_index do |val, i|
+        if i_arg < val
+            result.insert(i, i_arg)
             is_inserted = true
             break
         end
     end
-    result << i_arg if !is_inserted
+
+    # If it wasn't inserted, add it to the end
+    result << i_arg unless is_inserted
 end
 
 puts result
